9aff7734ecd9c3b380324ffbc13c17a8288b48b8
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-02-15 21:51:13 -0800

    Adding DownloadReq func to try if that helps with GOG images

--- client.go
+++ client.go
@@ -21,51 +21,41 @@ func NewClient(httpClient *http.Client, notify func(uint64, uint64)) *Client {
 	return &Client{httpClient: httpClient, notify: notify}
 }
 
-func (dolo *Client) sameContentLength(url *url.URL, dstFilename string) (bool, error) {
-
-	stat, err := os.Stat(dstFilename)
-	if err != nil && !os.IsNotExist(err) {
-		return false, err
-	}
-
-	if stat != nil {
-		resp, err := dolo.httpClient.Head(url.String())
-		if err != nil {
-			return false, err
-		}
-
-		defer resp.Body.Close()
-
-		if stat.Size() == resp.ContentLength {
-			return true, nil
-		}
-	}
-
-	return false, nil
-}
-
-func (dolo *Client) Download(url *url.URL, dstDir string, overwrite bool) error {
-
-	dstFilename := filepath.Join(dstDir, path.Base(url.String()))
-
-	if !overwrite {
-		sameCL, err := dolo.sameContentLength(url, dstFilename)
-		if err != nil {
-			return err
-		}
-
-		if sameCL {
-			log.Println("file already exists and has the same content length")
-			return nil
-		}
-	}
-
+func (dolo *Client) DownloadUrl(url *url.URL, dstDir string, overwrite bool) error {
 	resp, err := dolo.httpClient.Get(url.String())
 	if err != nil {
 		return err
 	}
 
+	return dolo.downloadResponse(resp, url, dstDir, overwrite)
+}
+
+func (dolo *Client) DownloadReq(req *http.Request, dstDir string, overwrite bool) error {
+	resp, err := dolo.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	return dolo.downloadResponse(resp, req.URL, dstDir, overwrite)
+}
+
+func (dolo *Client) downloadResponse(resp *http.Response, url *url.URL, dstDir string, overwrite bool) error {
 	defer resp.Body.Close()
+
+	dstFilename := filepath.Join(dstDir, path.Base(url.String()))
+
+	if !overwrite {
+		stat, err := os.Stat(dstFilename)
+		if err != nil && !os.IsNotExist(err) {
+			return err
+		}
+
+		if stat != nil {
+			if stat.Size() == resp.ContentLength {
+				log.Println("file already exists and has the same content length")
+				return nil
+			}
+		}
+	}
 
 	dstFile, err := os.Create(dstFilename + downloadExt)
 	if err != nil {
