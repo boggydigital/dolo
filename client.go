91a6a28debfae18632e66d113598e81192a6bf62
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-09-27 18:08:01 -0700

    Bumping UA

--- client.go
+++ client.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	DefaultUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+	DefaultUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.0 Safari/605.1.15"
 )
 
 type Client struct {
